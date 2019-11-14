66b4c5085322846241cc13ee7f383b41ffef1cab
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2019-11-14 14:58:52 +0000

    write test for model and get endpoint

--- __test__/server.test.js
+++ __test__/server.test.js
@@ -7,5 +7,24 @@ describe('server', () => {
             const response = await request(server).get('/')
             expect(response.status).toBe(200)
         })
+
+        test('should return 200 OK with ES6 promise', () => {
+            return request(server).get('/')
+                .then(response => {
+                    expect(response.status).toBe(200)
+                })
+        })
+
+        test('should return 200 OK with supertest syntax', () => {
+            return request(server).get('/')
+                .expect(200)
+                .expect('Content-Type', /json/)
+        })
     })
+
+    // describe('[POST] / endpoint', () => {
+    //     test('should return 200 OK', async () => {
+
+    //     }) 
+    // })
 })
