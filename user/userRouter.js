cce28e777fc95b7409db7e92db79a3b5b69e3238
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2019-11-14 13:04:05 +0000

    create endpoint to get all users and register new user

---
+++ user/userRouter.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const bcrypt = require('bcrypt')
+const db = require('./userModel')
+const router = express.Router()
+
+router.get('/', (req, res) => {
+    db.getUsers()
+    .then(users => {
+        res.status(200).json(users)
+    })
+    .catch(err => {
+        res.status(400).json({
+            message: 'Cannot get users' + err.message
+        })
+    })
+})
+
+router.post('/register', (req, res) => {
+    let user = req.body;
+    const hash = bcrypt.hashSync(user.password, 10)
+    user.password = hash;
+
+    db.addUser(user)
+    .then(user => {
+        res.status(200).json(user)
+    })
+    .catch(err => {
+        res.status(500).json({
+            message: 'Couldnt register user' + err.message
+        })
+    })
+})
