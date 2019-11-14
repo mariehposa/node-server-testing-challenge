8b3b437ad65829da9fe187f59a142ca6623f0eba
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2019-11-14 12:19:53 +0000

    create cleanup

--- data/migrations/20191114120718_create-table.js
+++ data/migrations/20191114120718_create-table.js
@@ -1,8 +1,16 @@
 
 exports.up = function(knex) {
-  
+    return knex.schema
+    .createTable('project', table => {
+        table.increments('id')
+        table.text('username', 128)
+          .notNullable()
+        table.string('password')
+          .notNullable()
+    })
 };
 
 exports.down = function(knex) {
-  
+    return knex.schema
+        .dropTableIfExists('project')
 };
